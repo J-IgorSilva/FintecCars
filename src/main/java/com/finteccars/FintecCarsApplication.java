f66b3bcf5dac5ce04fb16950ab88bf5d004d353e
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-27 14:11:16 -0300

    Iniciando Projeto FintecCars

---
+++ src/main/java/com/finteccars/FintecCarsApplication.java
@@ -0,0 +1,13 @@
+package com.finteccars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintecCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintecCarsApplication.class, args);
+	}
+
+}
