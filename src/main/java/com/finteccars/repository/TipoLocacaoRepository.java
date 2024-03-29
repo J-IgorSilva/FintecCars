acd3dbbda4e0ea1a42e4fac3b3717e417dd268cf
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:38:48 -0300

    feat(Implementação repository para persistencia e recuperação da entidade TipoLocacao)

---
+++ src/main/java/com/finteccars/repository/TipoLocacaoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.TipoLocacao;
+
+public interface TipoLocacaoRepository extends JpaRepository<TipoLocacao,Integer>{
+
+}
