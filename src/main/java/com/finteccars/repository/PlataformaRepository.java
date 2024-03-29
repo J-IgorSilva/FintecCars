a265f5cd01ec49835d3acc1e7a4b1c755af91436
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:37:34 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Plataforma)

---
+++ src/main/java/com/finteccars/repository/PlataformaRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Plataforma;
+
+public interface PlataformaRepository extends JpaRepository<Plataforma,Integer> {
+
+}
