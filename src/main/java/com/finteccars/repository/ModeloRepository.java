f5052cbc9f72d0b0edff0476b1c98cb6611e7b25
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:37:06 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Modelo)

---
+++ src/main/java/com/finteccars/repository/ModeloRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Modelo;
+
+public interface ModeloRepository extends JpaRepository<Modelo,Integer>{
+
+}
