3d95309cbd3e717600bf35b304b87b6523f88486
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 10:49:46 -0300

    feat(Criação da entidade Marca)

---
+++ src/main/java/com/finteccars/model/Marca.java
@@ -0,0 +1,20 @@
+package com.finteccars.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "marca")
+public class Marca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_marca", nullable = false)
+    private Integer idMarca;
+
+    @Column(name = "nome_marca", nullable = false, length = 100)
+    private String nomeMarca;
+}
