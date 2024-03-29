7121cacd6e7f7080cd0cae6a4bf3dd0fa889ff51
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:36:35 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Locacao)

---
+++ src/main/java/com/finteccars/repository/LocacaoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Locacao;
+
+public interface LocacaoRepository extends JpaRepository<Locacao,Integer>{
+
+}
