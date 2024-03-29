e938886625ebbcd3d389b25eb6330e3f661cbc60
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:35:22 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Documento)

---
+++ src/main/java/com/finteccars/repository/DocumentoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Documento;
+
+public interface DocumentoRepository extends JpaRepository<Documento,Integer>{
+
+}
