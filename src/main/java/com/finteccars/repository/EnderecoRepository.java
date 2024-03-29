deee73d2f65703217f664c3385c236b3de74b1ea
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:35:42 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Endereco)

---
+++ src/main/java/com/finteccars/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco,Integer>{
+
+}
