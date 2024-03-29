894ac28c30b4107034a876ee0504f84b2062b4ac
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:38:59 -0300

    feat(Implementação repository para persistencia e recuperação da entidade TipoLogadouro)

---
+++ src/main/java/com/finteccars/repository/TipoLogadouroRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.TipoLogadouro;
+
+public interface TipoLogadouroRepository extends JpaRepository<TipoLogadouro,Integer>{
+
+}
