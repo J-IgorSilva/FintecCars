7cf87845a17e4860d565fde800975547649ab4a6
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:35:08 -0300

    feat(Implementação repository para persistencia e recuperação da entidade cartao)

---
+++ src/main/java/com/finteccars/repository/CartaoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Cartao;
+
+public interface CartaoRepository extends JpaRepository<Cartao,Integer>{
+
+}
