c030c0671519f92130f2032ae44bb6cd368b8f99
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:36:15 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Imagem)

---
+++ src/main/java/com/finteccars/repository/ImagemRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Imagem;
+
+public interface ImagemRepository extends JpaRepository<Imagem,Integer>{
+
+}
