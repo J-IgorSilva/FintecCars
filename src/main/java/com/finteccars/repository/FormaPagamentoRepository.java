491cb1d8ea02c6b33a9a0d6e0885e9d82655a75f
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:36:04 -0300

    feat(Implementação repository para persistencia e recuperação da entidade FormaPagamento)

---
+++ src/main/java/com/finteccars/repository/FormaPagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.FormaPagamento;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento,Integer>{
+
+}
