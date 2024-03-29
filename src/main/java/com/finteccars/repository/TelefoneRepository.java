fa4b00f752805e9ca3bb500b185476186e3c046e
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:37:52 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Telefone)

---
+++ src/main/java/com/finteccars/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Telefone;
+
+public interface TelefoneRepository extends JpaRepository<Telefone,Integer>{
+
+}
