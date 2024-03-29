c4577697dc8881d527522e9f89052a9c418e8696
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:36:46 -0300

    feat(Implementação repository para persistencia e recuperação da entidade Marca)

---
+++ src/main/java/com/finteccars/repository/MarcaRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.Marca;
+
+public interface MarcaRepository extends JpaRepository<Marca,Integer>{
+
+}
