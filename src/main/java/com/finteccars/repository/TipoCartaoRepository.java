ea3434711f9b965fa8d5413fb26f1f340f41db9d
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 11:38:06 -0300

    feat(Implementação repository para persistencia e recuperação da entidade TipoCartao)

---
+++ src/main/java/com/finteccars/repository/TipoCartaoRepository.java
@@ -0,0 +1,9 @@
+package com.finteccars.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.finteccars.model.TipoCartao;
+
+public interface TipoCartaoRepository extends JpaRepository<TipoCartao,Integer>{
+
+}
