78aaba9be30b7db2b23e8996285ed71f4938c80e
J-IgorSilva <aleatoria8821@gmail.com>
2024-03-29 10:53:26 -0300

    feat(Criação da entidade Usuario)

---
+++ src/main/java/com/finteccars/model/Usuario.java
@@ -0,0 +1,34 @@
+package com.finteccars.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario", nullable = false)
+    private Integer idUsuario;
+
+    @Column(name = "email", nullable = false, length = 130)
+    private String email;
+
+    @Column(name = "ultima_atualizacao", nullable = false)
+    private Date ultmaAtualizacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pessoa", nullable = false)
+    private Pessoa pessoa;
+
+}
