4e8f9de22fee0bcef3d4e57445be0d908ff4f079
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:38:27 -0300

    fix(TelefoneRepository)adiciono anotação para gerenciamentod a classe telefonerepository.

--- src/main/java/com/finteccars/repository/TelefoneRepository.java
+++ src/main/java/com/finteccars/repository/TelefoneRepository.java
@@ -1,9 +1,10 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.Telefone;
-
+@Repository
 public interface TelefoneRepository extends JpaRepository<Telefone,Integer>{
 
 }
