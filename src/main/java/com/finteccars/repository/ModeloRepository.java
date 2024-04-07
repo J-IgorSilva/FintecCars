478925c803e7c0a649a20734c1ef749326d22fa7
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:36:11 -0300

    fix(ModeloRepository)adiciono anotação para gerenciamento da classe modelo repositorio.

--- src/main/java/com/finteccars/repository/ModeloRepository.java
+++ src/main/java/com/finteccars/repository/ModeloRepository.java
@@ -1,9 +1,10 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.Modelo;
-
+@Repository
 public interface ModeloRepository extends JpaRepository<Modelo,Integer>{
 
 }
