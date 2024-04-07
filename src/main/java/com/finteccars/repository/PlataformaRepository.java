b7628373d6fd7ba6d84ebdff54e57beaf8480e7e
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:37:37 -0300

    fix(PlataformaRepository)adiciono anotação para gerenciamento da classe Plataforma repositorio

--- src/main/java/com/finteccars/repository/PlataformaRepository.java
+++ src/main/java/com/finteccars/repository/PlataformaRepository.java
@@ -1,9 +1,10 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.Plataforma;
-
+@Repository
 public interface PlataformaRepository extends JpaRepository<Plataforma,Integer> {
 
 }
