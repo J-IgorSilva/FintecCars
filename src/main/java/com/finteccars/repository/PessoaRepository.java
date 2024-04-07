d6ea6f2024abd43df3e5b6c263398691b10b11a2
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:36:52 -0300

    fix(Pessoarepository) adiciono anotação para gerenciamento da classe pessoa repositorio.

--- src/main/java/com/finteccars/repository/PessoaRepository.java
+++ src/main/java/com/finteccars/repository/PessoaRepository.java
@@ -1,9 +1,11 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.Pessoa;
 
-public interface PessoaRepository extends JpaRepository<Pessoa,Integer>{
-
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+    Pessoa findByNomePessoa(String nome);
 }
