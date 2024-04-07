d53abca4bbb14931784c1d5d14472f8ff7c36482
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:34:31 -0300

    ifx(Locacao)adiciono anotação para gerenciamento da classe repositorio

--- src/main/java/com/finteccars/repository/LocacaoRepository.java
+++ src/main/java/com/finteccars/repository/LocacaoRepository.java
@@ -1,9 +1,10 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.Locacao;
-
+@Repository
 public interface LocacaoRepository extends JpaRepository<Locacao,Integer>{
 
 }
