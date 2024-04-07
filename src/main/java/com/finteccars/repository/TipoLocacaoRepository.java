24faa5101bd80bd1470dae0bd3088def11676d54
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:41:14 -0300

    fix(TipoLocacaoRepository)adiciono anotação para gereciamento da classe TipoLocacaoRepository

--- src/main/java/com/finteccars/repository/TipoLocacaoRepository.java
+++ src/main/java/com/finteccars/repository/TipoLocacaoRepository.java
@@ -1,9 +1,10 @@
 package com.finteccars.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.finteccars.model.TipoLocacao;
-
+@Repository
 public interface TipoLocacaoRepository extends JpaRepository<TipoLocacao,Integer>{
 
 }
