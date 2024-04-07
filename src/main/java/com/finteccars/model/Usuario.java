8a3a108d9382081b4dbc876c5927396a5f70b5aa
J-IgorSilva <aleatoria8821@gmail.com>
2024-04-07 15:30:50 -0300

    fix(Usuario)alteração de campo para mudaça de comportamento da entidade

--- src/main/java/com/finteccars/model/Usuario.java
+++ src/main/java/com/finteccars/model/Usuario.java
@@ -2,6 +2,7 @@ package com.finteccars.model;
 
 import java.util.Date;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,15 +26,54 @@ public class Usuario {
     @Column(name = "email", nullable = false, length = 130)
     private String email;
 
+    @Column(name = "nome", nullable = false, length = 80)
+    private String nome;
+
     @Column(name = "ultima_atualizacao", nullable = false)
     private Date ultmaAtualizacao;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "senha", nullable = false, length = 255)
+    private String senha;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "id_pessoa", nullable = false)
     private Pessoa idpessoa;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_token_expiracao", nullable = false)
+    private TokenExpiracao idtokenExpiracao;
+
+    @PrePersist
+    public void prePersist() {
+        this.ultmaAtualizacao = new Date();
+    }
+
+    public TokenExpiracao getTokenExpiracao() {
+        return idtokenExpiracao;
+    }
+
+    public void setTokenExpiracao(TokenExpiracao tokenExpiracao) {
+        this.idtokenExpiracao = tokenExpiracao;
+    }
+    
     public Integer getIdUsuario() {
         return idUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Pessoa getIdpessoa() {
+        return idpessoa;
+    }
+
+    public void setIdpessoa(Pessoa idpessoa) {
+        this.idpessoa = idpessoa;
     }
 
     public void setIdUsuario(Integer idUsuario) {
@@ -55,23 +96,33 @@ public class Usuario {
         this.ultmaAtualizacao = ultmaAtualizacao;
     }
 
-    public Pessoa getPessoa() {
-        return pessoa;
+    public String getNome() {
+        return nome;
     }
 
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public Usuario(Integer idUsuario, String email, Date ultmaAtualizacao, Pessoa pessoa) {
+    public Usuario(TokenExpiracao idtokenExpiracao, Integer idUsuario, String nome, String email, Date ultmaAtualizacao, String senha, Pessoa idpessoa) {
         this.idUsuario = idUsuario;
         this.email = email;
         this.ultmaAtualizacao = ultmaAtualizacao;
-        this.pessoa = pessoa;
+        this.senha = senha;
+        this.idpessoa = idpessoa;
+        this.nome = nome;
+        this.idtokenExpiracao=idtokenExpiracao;
     }
+
+    /*public Usuario(Integer idUsuario, String nome, String email, String senha, Pessoa idpessoa) {
+        this.idUsuario = idUsuario;
+        this.email = email;
+        this.senha = senha;
+        this.idpessoa = idpessoa;
+        this.nome = nome;
+    }*/
 
     public Usuario() {
     }
 
-    
 }
